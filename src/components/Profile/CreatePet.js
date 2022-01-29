1661a2a4418afa0cfdcf0dddf5148e011bc504fe
Lawrence <lawrencesalinas@yahoo.com>
2022-01-29 14:41:12 -0500

    code push

--- src/components/Profile/CreatePet.js
+++ src/components/Profile/CreatePet.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Icon } from 'react-materialize'
-import {Row,Col, Button } from 'react-bootstrap'
+import {Row,Col, Button, Modal } from 'react-bootstrap'
 import './ProfilePets.css'
 
 
@@ -8,6 +8,10 @@ export default function PetForm(props) {
   // console.log('I AM USER',props);
   const [name, setName] = useState('')
   const [image, setImage]= useState();
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
 
   const createPet = (e) => {
@@ -36,22 +40,76 @@ export default function PetForm(props) {
       console.log(error);
     
     })
-
+    setShow(false)
   }
 
   return (
     <div>
+      <button class="bone">
+    <div class="c1"></div>
+    <div class="c2"></div>
+    <div class="c3"></div>
+    <div class="c4"></div>
+    <div class="b1">
+      <div class="b2">
+       Add Pet
+      </div>
+    </div>
+</button>
+      {/* <Button variant="primary" onClick={handleShow}>
+Add Pet
+</Button>
+      <Modal
+ show={show}
+ onHide={handleClose}
+ backdrop="static"
+ keyboard={false}
+>
+   <Modal.Header closeButton>
+   <Modal.Title>Add your Pet</Modal.Title>
+ </Modal.Header>
+
        <label>
-        Title
-        <input type="text" value={name} onChange={(evt) => setName(evt.target.value)}/>
-      </label>
+       Pet Name
+        <input type="text" value={name}   onChange={(evt) => setName(evt.target.value)}/>
+      </label> 
       <br/>
       <label>
-        Cover
         <input type="file" onChange={(evt) => setImage(evt.target.files[0])}/>
       </label>
       <br/>
-      <button onClick={() => createPet()}>New Book</button>
+       <Button onClick={() => createPet()}>Add Pet</Button>
+       </Modal> */}
+    
     </div>
   );
 }
+
+{/* <Button variant="primary" onClick={handleShow}>
+Add Pet
+</Button>
+
+<Modal
+ show={show}
+ onHide={handleClose}
+ backdrop="static"
+ keyboard={false}
+>
+ <Modal.Header closeButton>
+   <Modal.Title>Add your Pet</Modal.Title>
+ </Modal.Header>
+ <Modal.Body>
+ A profile photo that shows your pet.
+ <input type="file" onChange={(evt) => setImage(evt.target.files[0])}/>
+ <label>
+ Pet Name:
+ <input type="text" value={name} onChange={(evt) => setName(evt.target.value)}/>
+</label>
+ </Modal.Body>
+ <Modal.Footer>
+   <Button variant="secondary" onClick={handleClose}>
+     Close
+   </Button>
+   <Button onClick={() => createPet()}>Add Pet</Button>
+ </Modal.Footer>
+</Modal> */}
--- src/screens/PetScreen.js
+++ src/screens/PetScreen.js
@@ -1,37 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { getUsersPets } from '../api/pets';
-import ProfilePets from '../components/Profile/ProfilePets';
-import CreatePets from '../components/Profile/CreatePet'
-import './PetScreen.css'
-import Footer from '../components/Footer';
-import { Button } from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import { getUsersPets } from "../api/pets";
+import ProfilePets from "../components/Profile/ProfilePets";
+import CreatePets from "../components/Profile/CreatePet";
+import "./PetScreen.css";
+import Footer from "../components/Footer";
+import { Button } from "react-bootstrap";
 
 function PetScreen(props) {
-    const [userPets, setUserPets] = useState([]);
-    const [showAddPet, setShowAddPet] = useState(false)
-    const [trigger, setTrigger] = useState(false)
-    
+  const [userPets, setUserPets] = useState([]);
+  const [trigger, setTrigger] = useState(false);
+  const [show, setShow] = useState(false);
 
-    useEffect(() => {
-      getUsersPets(props.user)
-        .then((user) => {
-          console.log('this is profile', user);
 
-          let userPet = user.data.user.pets_owned;
-          setUserPets(userPet);
-   
-        })
-        .catch((err) => console.error(err));
-    }, [trigger]);
-  console.log('HELLLOOOOOOOOOOOOO',userPets)
+  useEffect(() => {
+    getUsersPets(props.user)
+      .then((user) => {
+        console.log("this is profile", user);
+
+        let userPet = user.data.user.pets_owned;
+        setUserPets(userPet);
+      })
+      .catch((err) => console.error(err));
+  }, [trigger]);
+  console.log("HELLLOOOOOOOOOOOOO", userPets);
   return (
-  <div className='pet_screen'>
-    <ProfilePets myPets={userPets} user={props.user} setTrigger={setTrigger}/>
-    <CreatePets user={props.user} setTrigger={setTrigger}/>
-    {/* <Button onClick={() => setShowAddPet(!showAddPet)} className = 'banner_searchButton' variant='outlined' >{showAddPet? "Hide" : "Search Dates"}</Button> */}
-
-  </div>
-  )
+    <div className="pet_screen">
+      <ProfilePets
+        myPets={userPets}
+        user={props.user}
+        setTrigger={setTrigger}
+      />
+      <div className="createpets">
+        <CreatePets user={props.user} setTrigger={setTrigger} />
+      </div>
+    </div>
+  );
 }
 
 export default PetScreen;
